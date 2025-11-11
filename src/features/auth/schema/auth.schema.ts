55cb08dab3f33522748a173025bf58fdd482bd7e
Maina <mainaedwin913@gmail.com>
2025-11-11 11:26:59 +0300

    updates: auth

---
+++ src/features/auth/schema/auth.schema.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod'
+
+export const USER_TYPE = ['employer', 'member'] as const;
+export type UserType = typeof USER_TYPE[number];
+
+
+export const loginSchema = z.object({
+    email_address: z
+        .string()
+        .min(1, 'Email or username is required')
+        .email('Invalid email address'),
+    password: z
+        .string()
+        .min(6, 'Password must be at least 6 characters long'),
+    user_type: z
+        .enum(USER_TYPE),
+})
+export type LoginData = z.infer<typeof loginSchema>
+
+
+export const resetPasswordSchema = z.object({
+    email: z
+        .string()
+        .min(1, 'Email or username is required')
+        .email('Invalid email address'),
+})
+export type ResetPasswordData = z.infer<typeof resetPasswordSchema>
+
+export const updatePasswordSchema = z.object({
+    customer_ref: z
+        .string(),
+    password: z
+        .string()
+        .min(6, 'Password must be at least 6 characters long'),
+})
+export type updatePasswordData = z.infer<typeof updatePasswordSchema>
+
---
+++ src/features/auth/types/auth.ts
@@ -0,0 +1,60 @@
+import type { UserType } from "../schema/auth.schema"
+
+export interface LoginResponse {
+    status: number
+    status_code: number
+    message: string
+    otp_generated: string
+    token: string
+    member_status: number
+}
+
+export interface LogoutResponse {
+    status: number
+    message: string
+}
+
+export interface ResetPasswordResponse {
+    status_code: number
+    message?: string
+    customer_ref?: string
+    token?: string
+    member_status?: number
+    member_token?: string
+    member_no?: string
+}
+
+export interface UpdatePasswordResponse {
+    status_code: number
+    message?: string
+}
+
+export interface OtpResponse {
+    status: number
+    message: string
+    token: string
+    member_status: number
+    accounts_count: number
+}
+
+export interface ResendOtpResponse {
+    status: number
+    message: string
+}
+
+export type SessionUser = {
+    member_no?: string
+    email: string
+    role: UserType
+    custom_ref?: string
+}
+
+export type SessionData = {
+    is_authed: boolean
+    login_token: string | null
+    auth_token: string | null
+    user: SessionUser | null
+    lastTouch?: number
+    expiresAt?: number
+    csrfToken?: string
+}
