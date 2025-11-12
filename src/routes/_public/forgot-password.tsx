6992a53e2c548c57334e51a1218e48d3fa8deffa
Maina <mainaedwin913@gmail.com>
2025-11-12 12:43:28 +0300

    updates

--- src/routes/_public/forgot-password.tsx
+++ src/routes/_public/forgot-password.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import ForgotPasswordPage from '@/features/auth/components/ForgotPasswordPage'
+import { ForgotPasswordPage } from '@/features/auth/components/ForgotPasswordPage'
 
 
 export const Route = createFileRoute('/_public/forgot-password')({
